d25cc7d95561ba9ca192682c84dc79a1f9e33f73
Aalok Kumar <aaloksah766626@gmail.com>
2025-12-23 14:39:45 +0530

    notifications reversal

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,9 +11,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-export const app: FirebaseApp = getApps().length
-  ? getApps()[0]
-  : initializeApp(firebaseConfig);
+const app: FirebaseApp = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
